802da012d11762a716413f915484de6f285a0a9d
yossibank <yossibank@yahoo.co.jp>
2024-04-28 22:54:17 +0900

    test commit

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "BuildTools",
     dependencies: [
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.53.8")
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.53.7")
     ],
     targets: [
         .target(
