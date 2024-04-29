560518cc0cf1f9c8ca5f8005cdb2462f81b31680
yossibank <yossibank@yahoo.co.jp>
2024-04-29 10:04:52 +0900

    add SnapKit library

--- Package/Package.swift
+++ Package/Package.swift
@@ -3,45 +3,7 @@
 
 import PackageDescription
 
-// MARK: - Package
-
-let sample = Target.target(
-    name: "Sample"
-)
-
-let mock = Target.target(
-    name: "Mock",
-    dependencies: [sample]
-)
-
-// MARK: - Test Package
-
-let sampleTests = Target.testTarget(
-    name: "SampleTests",
-    dependencies: [sample, mock]
-)
-
-// MARK: - Target
-
-let package = Package.package(
-    name: "Package",
-    platforms: [
-        .iOS(.v17)
-    ],
-    dependencies: [
-        .package(
-            url: "https://github.com/nicklockwood/SwiftFormat",
-            from: "0.53.8"
-        )
-    ],
-    targets: [
-        sample,
-        mock
-    ],
-    testTargets: [
-        sampleTests
-    ]
-)
+// MARK: - Extension
 
 extension Target {
     private var dependency: Target.Dependency {
@@ -102,3 +64,55 @@ extension Package {
         )
     }
 }
+
+// MARK: - Library
+
+let snapkit = Target.Dependency.product(
+    name: "SnapKit",
+    package: "SnapKit"
+)
+
+// MARK: - Package
+
+let sample = Target.target(
+    name: "Sample",
+    dependencies: [snapkit]
+)
+
+let mock = Target.target(
+    name: "Mock",
+    dependencies: [sample]
+)
+
+// MARK: - Test Package
+
+let sampleTests = Target.testTarget(
+    name: "SampleTests",
+    dependencies: [sample, mock]
+)
+
+// MARK: - Target
+
+let package = Package.package(
+    name: "Package",
+    platforms: [
+        .iOS(.v17)
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/nicklockwood/SwiftFormat",
+            from: "0.53.8"
+        ),
+        .package(
+            url: "https://github.com/SnapKit/SnapKit.git",
+            from: "5.7.0"
+        )
+    ],
+    targets: [
+        sample,
+        mock
+    ],
+    testTargets: [
+        sampleTests
+    ]
+)
